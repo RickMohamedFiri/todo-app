338dd6e1d4fe076e1b0a673827c72b645c2448eb
Rick Mohamed <rickmohaa@gmail.com>
2023-09-27 09:00:01 +0300

    new commit

--- components/Todo.jsx
+++ components/Todo.jsx
@@ -44,7 +44,7 @@ const Cart = () => {
         })
             .then(response => {
                 if (!response.ok) {
-                    throw new Error('Network response was not ok');
+                    throw new Error('Network response was not ok!');
                 }
                 return response.json();
             })
